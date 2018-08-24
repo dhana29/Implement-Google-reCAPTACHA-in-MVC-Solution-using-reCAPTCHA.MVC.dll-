4497ea02ec93be10dd3ef88025d6321facd94788
Dhana <mcsesas@gmail.com>
2018-08-24 18:41:50 -0400

    Add files via upload

--- Google reCAPTACHA in MVC/Models/CaptchaResponse.cs
+++ Google reCAPTACHA in MVC/Models/CaptchaResponse.cs
@@ -1,1 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 
+namespace Google_reCAPTACHA_in_MVC.Models
+{
+    public class CaptchaResponse
+    {
+        [JsonProperty("success")]
+        public bool Success
+        {
+            get;
+            set;
+        }
+        [JsonProperty("error-codes")]
+        public List<string> ErrorMessage
+        {
+            get;
+            set;
+        }  
+    }
+}
